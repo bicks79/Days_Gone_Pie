3796df337dc61fee223dead4793cb38d3ab6f757
bicks79 <nicholson.lcp@gmail.com>
2021-02-17 18:15:04 +0000

    added recipe cards to recipe.html

--- static/js/script.js
+++ static/js/script.js
@@ -2,3 +2,7 @@ $("#tagline").hide()
 $("#tagline").fadeIn();
 $(".button-collapse").sideNav();
 $('.modal-trigger').leanModal();
+
+  $(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
